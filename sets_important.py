a42016b4e0bf5adb2df2cbaa8edf2793b69384b1
naitax <73228371+naitax@users.noreply.github.com>
2021-09-29 15:15:44 +0100

    Create sets_important.py
    Sets can be combined using mathematical operations.
    The union operator | combines two sets to form a new one containing items in either.
    The intersection operator & gets items only in both.
    The difference operator - gets items in the first set but not in the second.
    The symmetric difference operator ^ gets items in either set, but not both.

---
+++ sets_important.py
@@ -0,0 +1,8 @@
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second) #combines two sets
+print(first & second) #gets items only in both
+print(first - second) #gets in the first but not in the second
+print(second - first)
+print(first ^ second) #items in either set but not both
